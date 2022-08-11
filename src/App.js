f93f5d65727a29b87be08d1b2440ad012c69193f
Eduardo <eduardocastillogimenez1@gmail.com>
2022-08-10 22:30:44 -0400

    cambios para ver estilos de antd

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import { BrowserRouter as Router, Switch,Route } from "react-router-dom";
-import './App.less';
+
+import 'antd/dist/antd.min.css';
 //import './app.css';
 
 import Home from './views/home';
